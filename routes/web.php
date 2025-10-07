bce2c9fd5f2e7289b33f9dd19c9a9eb3c5a4b3ee
Alex <b1c2d3@yandex.ru>
2025-10-07 22:14:00 +1000

    register

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'login']);
+Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/{any}', function () {
     return view('index');
